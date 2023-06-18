cedb9243f07577f4d074c5a4864d36c8f365056a
Marie-Schmit <marie.schmit3003@gmail.com>
2023-06-18 14:28:18 +0100

    Refactor(JsonFiles): Creation of new class JsonFiles
    Create class to save functions relative to JSON files management (ex: saveJobJson, saveSerevrJson, serversFromJson, jobsFromJson).

---
+++ src/ComparativeGenomics/FileHandling/JsonFiles.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ComparativeGenomics.FileHandling;
+
+/**
+ *
+ * @author marie
+ * Manage creation and retrieval of information from server Json files.
+ * Encryption or decryption of the Json, getting or saving jobs or servers in those files.
+ */
+public class JsonFiles {
+    
+}
+
+//Constructor creates and encrypt the json files
+
+// Add saveJobJson
+// saveServerJson
+//serversFromJson
+//JobsFromJson
+
+
